cc072e18bffdf15088c1bdf8aa952eeaa0fb212e
Anwesh-18 <anweshbarpanda.02@gmail.com>
2025-11-18 07:19:08 +0530

    Day-9

---
+++ 18-11-2025/leetcode_717.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        left = 0
+
+        while left<len(bits):
+            if bits[left] == 0:
+                left+=1
+            else:
+                left += 2
+        
+        return left == len(bits)-1
