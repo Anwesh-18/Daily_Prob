424930d0243585efdb24073aaea7be6f8f013462
Anwesh-18 <anweshbarpanda.02@gmail.com>
2025-11-22 21:43:30 +0530

    Dp ongoing

---
+++ 22-11-2025/letcode_3190.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        ops = 0
+        for num in nums:
+            if (num%3)!=0:
+                ops+=1
+        return ops  
